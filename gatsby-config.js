18843f182acbfdf2aeb1ee9e43392dd00b110531
WelGomws <wellingtond.gomes@hotmail.com>
2021-02-05 14:20:31 -0300

    feat: change details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Tech Talk`,
+    description: ``,
     author: `@gatsbyjs`,
+    copyright: `© 2020 Badico Cloud. All rights reserved.`,
+    socialMedia: {
+      github: "https://github.com/badico-cloud-hub",
+      linkedin: "https://www.linkedin.com/company/badico/",
+      podcast: "https://anchor.fm/dogwalktalk",
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,18 +19,28 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Nunito\:400, 500, 700`, // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Tech Talk`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
